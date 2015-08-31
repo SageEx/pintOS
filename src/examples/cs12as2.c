3c94cb83b2694696e010263da1c2e26c06f05321
Arindam Banerjee <arindam.banerjee.790@gmail.com>
2015-09-01 00:25:43 +0530

    Assignment-2

---
+++ src/examples/cs12as2.c
@@ -0,0 +1,17 @@
+// #include <stdio.h>
+// #include <syscall.h>
+
+// int
+// main (void)
+// {
+  
+//   printf ("\nHello Arindam - 1301CS12\n");
+
+//   return EXIT_SUCCESS;
+// }
+
+void
+as1301CS12(void)
+{
+    printf("\nHello Arindam - 1301CS12\n");
+}
