4384ddc646bb7a8af17922d116b80b669cdd7569
Arindam Banerjee <arindam.banerjee.790@gmail.com>
2015-11-10 16:29:07 +0530

    Endsem Done - HE HA HA :) :P git add --all

--- src/examples/cs12as2.c
+++ src/examples/cs12as2.c
@@ -3,71 +3,134 @@
 #include "../threads/synch.h"
 #include "../threads/thread.h"
 
-struct semaphore Tobacco,Paper,Matches,Smoking;
+struct semaphore milk,water,tea,sugar;
 int curr=0;
-int no=40;
+int no=400;
 
 void
-agent(){
+mother(){
     while(no--){
         /* printf("Agent Running\n"); */
-        sema_down(&Smoking);
         if(curr==0){
             curr++;
-            curr%=3;
-            sema_up(&Tobacco);
+            curr%=4;
+            sema_up(&milk);
+            sema_up(&water);
+            sema_up(&sugar);
         }
         else if(curr==1){
             curr++;
-            curr%=3;
-            sema_up(&Paper);
+            curr%=4;
+            sema_up(&tea);
+            sema_up(&water);
+            sema_up(&sugar);
         }
         else if(curr==2){
             curr++;
-            curr%=3;
-            sema_up(&Matches);
+            curr%=4;
+            sema_up(&tea);
+            sema_up(&sugar);
+            sema_up(&milk);
+        }
+        else if(curr==3){
+            curr++;
+            curr%=4;
+            sema_up(&tea);
+            sema_up(&water);
+            sema_up(&milk);
         }
     }
 }
 
 void
-tob(){
+child1(){
     while(no--){
-        /* if(smoking==1 && curr==0){ */
-        /*     printf("Tobacco Man Got Paper & Matches - He is smoking "); */
-        /*     smoking=0; */
-        /*     /1* sem *1/ */
-        /* } */
-        printf("Tobacco Man Got Paper & Matches - He is smoking - He is HIGH !!\n ");
-        sema_up(&Smoking);
-        sema_down(&Tobacco);
+        bool a1 = sema_try_down(&tea);
+        bool a2 = sema_try_down(&sugar);
+        if(a1 && a2){
+            printf("Child 1 got tea & sugar.\n");
+            sema_try_down(&tea);
+            sema_try_down(&sugar);
+            sema_try_down(&water);
+            sema_try_down(&milk);
+            int j = thread_get_priority();
+            j--;
+            thread_set_priority(j);
+        }else if( a2 ){
+            sema_up(&tea);
+        } else if(a2){
+            sema_up(&sugar);
+        }
 
     }
 }
+
 void
-pap(){
+child2(){
     while(no--){
-        /* if(smoking==1 && curr==1){ */
-        /*     printf("Paper Man Got Tobacco & Matches - He is smoking "); */
-        /*     smoking=0; */
-        /*     /1* sem *1/ */
-        /* } */
-        printf("Paper Man Got Tobacco & Matches - He is smoking - He is HIGH !! \n");
-        sema_up(&Smoking);
-        sema_down(&Paper);
+
+        bool a1 = sema_try_down(&milk);
+        bool a2 = sema_try_down(&tea);
+        if(a1 && a2){
+            printf("Child 2 got tea & milk.\n");
+            sema_try_down(&tea);
+            sema_try_down(&sugar);
+            sema_try_down(&water);
+            sema_try_down(&milk);
+            int j = thread_get_priority();
+            j--;
+            thread_set_priority(j);
+        }else if( a2 ){
+            sema_up(&milk);
+        } else if(a2){
+            sema_up(&tea);
+        }
     }
 }
+
 void
-mat(){
+child3(){
     while(no--){
-        /* if(smoking==1 && curr==2){ */
-        /*     printf("Matches Man Got Paper & Tobacco - He is smoking "); */
-        /*     smoking=0; */
-        /*     /1* sem *1/ */
-        /* } */
-        printf("Matches Man Got Paper & Tobacco - He is smoking - He is HIGH !! \n");
-        sema_up(&Smoking);
-        sema_down(&Matches);
+
+        bool a1 = sema_try_down(&water);
+        bool a2 = sema_try_down(&sugar);
+        if(a1 && a2){
+            printf("Child 3 got water & sugar.\n");
+            sema_try_down(&tea);
+            sema_try_down(&sugar);
+            sema_try_down(&water);
+            sema_try_down(&milk);
+            int j = thread_get_priority();
+            j--;
+            thread_set_priority(j);
+        }else if( a2 ){
+            sema_up(&water);
+        } else if(a2){
+            sema_up(&sugar);
+        }
+    }
+}
+
+void
+child4(){
+    while(no--){
+
+        bool a1 = sema_try_down(&water);
+        bool a2 = sema_try_down(&milk);
+        if(a1 && a2){
+            printf("Child 4 got water & milk.\n");
+            sema_try_down(&tea);
+            sema_try_down(&sugar);
+            sema_try_down(&water);
+            sema_try_down(&milk);
+            int j = thread_get_priority();
+            j--;
+            thread_set_priority(j);
+        }else if( a2 ){
+            sema_up(&water);
+        } else if(a2){
+            sema_up(&milk);
+        }
     }
 }
 
@@ -75,13 +138,14 @@ void
 as1301CS12(void)
 {
     printf("\nHello Arindam - 1301CS12\n");
-    sema_init(&Smoking,0);
-    sema_init(&Tobacco,0);
-    sema_init(&Paper,0);
-    sema_init(&Matches,0);
-    thread_create("agent",5,&agent,"");
-    thread_create("Tobacco",5,&tob,"");
-    thread_create("Paper",5,&pap,"");
-    thread_create("Matches",5,&mat,"");
+    sema_init(&milk,0);
+    sema_init(&water,0);
+    sema_init(&tea,0);
+    sema_init(&sugar,0);
+    thread_create("Mother",33,&mother,"");
+    thread_create("child2",30,&child2,"");
+    thread_create("child1",31,&child1,"");
+    thread_create("child3",29,&child3,"");
+    thread_create("child4",28,&child4,"");
 
 }
